9d688bc88c65014d21647a9b32609ee72961e46c
Shayan Ahmadi <ahmadi.shayan64@gmail.com>
2018-04-21 18:45:23 +0000

    Completed!

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ var express=require("express");
 var app=express();
 
 var request=require("request");
+
 app.set("view engine","ejs");
 
+app.get("/",function(req,res){
+    res.render("search");
+})
+
 app.get("/results",function(req,res){
-    request("http://omdbapi.com/?s=iowa&apikey=thewdb",function(error,response,body){
+    console.log(req.query);
+    var query=req.query.search;
+    
+    request("http://omdbapi.com/?s="+query+"&apikey=thewdb",function(error,response,body){
         if(!error && response.statusCode==200){
         var data=JSON.parse(body);
-          
+        
         res.render("results",{data:data});
         }
     });
